87dbbe85da75f3438be4c85f148d46ecfbad2972
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2021-01-21 20:43:30 +0800

    同步代码

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import { getUserList } from '@/api/table'
 
 export default {
   filters: {
@@ -69,7 +69,7 @@ export default {
   methods: {
     fetchData() {
       this.listLoading = true
-      getList().then(response => {
+      getUserList().then(response => {
         this.list = response.data.items
         this.listLoading = false
       })
