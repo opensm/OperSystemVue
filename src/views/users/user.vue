3299d23a9b21bc4d026eced56f67a72bf75c4678
engine-oper <smxblysq@qq.com>
2021-04-25 16:59:40 +0800

    测试

--- src/views/users/user.vue
+++ src/views/users/user.vue
@@ -17,18 +17,15 @@
       >
         搜索
       </el-button>
-      <span v-if="this.post">
-        <el-button
-          class="filter-item"
-          style="margin-left: 10px;"
-          type="primary"
-          icon="el-icon-edit"
-          @click="handleCreate"
-        >
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        type="primary"
+        icon="el-icon-edit"
+        @click="handleCreate"
+      >
         新增
       </el-button>
-      </span>
-
     </div>
 
     <el-table
